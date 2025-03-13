798c8ce3e0ba10e5d05f117d4bc78f97b5cf5cf8
ub <drusmanbashir@gmail.com>
2025-03-13 01:28:08 +0000

    pip

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 from setuptools import setup, find_packages
+import os
+with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), encoding="utf-8") as f:
+    all_requirements = f.read().splitlines()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
+development_only = ["gudhi", "nurbspy"]
+
+# Filter out development-only requirements using partial string matching
+requirements = [
+    req for req in all_requirements 
+    if not any(dev_req in req for dev_req in development_only)
+]
 setup(
     name="label_analysis",
     version="0.1.0",
@@ -8,8 +20,7 @@ setup(
     description="A Python package for analyzing and processing labeled data.",
     url="https://github.com/yourusername/label_analysis",
     packages=find_packages(),
-    install_requires=[
-    ],
+    install_requires= requirements,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
